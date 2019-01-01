c28c2c3c69a0580c8bce0fa880b5fff87d89f19b
U-FLASH\alex <sashaanderson@users.noreply.github.com>
2019-01-01 11:34:42 -0500

    Move certain fixed sqlite connection properties from config into code.

--- oasis-ledger-server/src/main/java/oasisledger/server/AppConfig.java
+++ oasis-ledger-server/src/main/java/oasisledger/server/AppConfig.java
@@ -1,6 +1,7 @@
 package oasisledger.server;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableMap;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 
@@ -14,9 +15,15 @@ public class AppConfig extends Configuration {
     private DataSourceFactory database = new DataSourceFactory();
 
     @JsonProperty("database")
-    public void setDataSourceFactory(DataSourceFactory factory) {
-        this.database = factory;
+    public void setDataSourceFactory(DataSourceFactory database) {
+        database.setDriverClass(org.sqlite.JDBC.class.getName()); // org.sqlite.JDBC
+        database.setProperties(ImmutableMap.of(
+                "open_mode", "2" // SQLITE_OPEN_READWRITE, do not create if not exists
+        ));
+        database.setInitializationQuery("pragma foreign_keys = on");
+        this.database = database;
     }
+
     @JsonProperty("database")
     public DataSourceFactory getDataSourceFactory() {
         return database;
