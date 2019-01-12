027fb637136c61a0e2698e45f580854bf01bb31d
U-FLASH\alex <sashaanderson@users.noreply.github.com>
2019-01-12 06:22:01 -0500

    Shorten Sqlite exception message (for example unique constraint violation) instead of dumping full track trace.

--- oasis-ledger-server/src/main/java/oasisledger/server/AppExceptionMapper.java
+++ oasis-ledger-server/src/main/java/oasisledger/server/AppExceptionMapper.java
@@ -1,7 +1,9 @@
 package oasisledger.server;
 
+import org.jdbi.v3.core.statement.UnableToExecuteStatementException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sqlite.SQLiteException;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -10,13 +12,16 @@ import javax.ws.rs.ext.ExceptionMapper;
 
 public class AppExceptionMapper implements ExceptionMapper<Exception> {
 
-    private static final Logger logger = LoggerFactory.getLogger(AppExceptionMapper.class);
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
     public Response toResponse(Exception e) {
         String message;
         if (e instanceof WebApplicationException) {
             message = e.getMessage();
+        } else if (e instanceof UnableToExecuteStatementException && e.getCause() instanceof SQLiteException) {
+            message = e.getCause().getMessage();
+            // for example: [SQLITE_CONSTRAINT] Abort due to constraint violation (UNIQUE constraint failed: ...)
         } else {
             message = e.toString();
             logger.warn("Exception occurred: " + e.toString(), e);
