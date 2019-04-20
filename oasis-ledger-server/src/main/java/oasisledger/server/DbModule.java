dc58ff1579edc9b1c019f4a40a6e1033d9609019
U-FLASH\alex <sashaanderson@users.noreply.github.com>
2019-04-20 06:40:12 -0400

    feat(server): log debug SQL bindings from lazy beans as well

--- oasis-ledger-server/src/main/java/oasisledger/server/DbModule.java
+++ oasis-ledger-server/src/main/java/oasisledger/server/DbModule.java
@@ -3,8 +3,6 @@ package oasisledger.server;
 import com.google.common.reflect.ClassPath;
 import com.google.inject.AbstractModule;
 import org.jdbi.v3.core.Jdbi;
-import org.jdbi.v3.core.statement.SqlLogger;
-import org.jdbi.v3.core.statement.StatementContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,18 +11,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DbModule extends AbstractModule implements SqlLogger {
+public class DbModule extends AbstractModule {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    private final String EOL = System.lineSeparator();
 
     private final Jdbi jdbi;
     private final List<Class<?>> repoClasses;
 
     public DbModule(Jdbi jdbi) throws IOException {
         this.jdbi = jdbi;
-        jdbi.setSqlLogger(this);
+        jdbi.setSqlLogger(new oasisledger.server.data.SqlLogger());
 
         String packageName = getClass().getPackage().getName() + ".data.repo";
         this.repoClasses = ClassPath.from(ClassLoader.getSystemClassLoader())
@@ -42,14 +38,4 @@ public class DbModule extends AbstractModule implements SqlLogger {
         repoClasses.forEach(c -> bind(c));
     }
 
-    @Override
-    public void logBeforeExecution(StatementContext ctx) {
-        String indent = EOL + "  ";
-        logger.debug("SQL = " + EOL
-                + indent + String.join(indent, ctx.getRawSql().trim().split("\\r?\\n"))
-                + EOL);
-        if (!ctx.getBinding().isEmpty()) {
-            logger.debug("Bindings = " + ctx.getBinding().toString());
-        }
-    }
 }
---
+++ oasis-ledger-server/src/main/java/oasisledger/server/data/SqlLogger.java
@@ -0,0 +1,38 @@
+package oasisledger.server.data;
+
+import org.jdbi.v3.core.statement.StatementContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SqlLogger implements org.jdbi.v3.core.statement.SqlLogger {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final String EOL = System.lineSeparator();
+
+    @Override
+    public void logBeforeExecution(StatementContext ctx) {
+        if (logger.isDebugEnabled()) {
+            String sql = ctx.getRawSql();
+            String indent = EOL + "  ";
+            logger.debug("SQL = " + EOL
+                    + indent + String.join(indent, sql.trim().split("\\r?\\n"))
+                    + EOL);
+            if (!ctx.getBinding().isEmpty()) {
+                logger.debug("Bindings = " + ctx.getBinding().toString());
+                if (sql.contains(":")) {
+                    Pattern p = Pattern.compile(":\\w+");
+                    Matcher m = p.matcher(sql);
+                    while (m.find()) {
+                        logger.debug("Binding " + m.group() + " = " + ctx.getBinding()
+                                .findForName(m.group().substring(1), ctx)
+                                .orElse(null));
+                    }
+                }
+            }
+        }
+    }
+}
