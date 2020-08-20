e75aeada0a13fcf7f3be5e74980ea6d883782735
U-FLASH\alex <sashaanderson@users.noreply.github.com>
2020-08-20 06:54:51 -0400

    style(db): Fixed missing comma, oops

--- oasis-ledger-db/src/tables/account_type.sql
+++ oasis-ledger-db/src/tables/account_type.sql
@@ -7,7 +7,7 @@ CREATE TABLE account_type (
   audit_user_id INTEGER NOT NULL DEFAULT 1,
   audit_ts      INTEGER NOT NULL DEFAULT (strftime('%s','now') * 1000),
 
-  CONSTRAINT account_type_pk PRIMARY KEY (account_type_id)
+  CONSTRAINT account_type_pk PRIMARY KEY (account_type_id),
   CONSTRAINT account_group_uq_atc UNIQUE (account_type_code),
   CONSTRAINT account_group_ck_s CHECK (sign IN (-1, 1)),
   CONSTRAINT account_group_fk_auid FOREIGN KEY (audit_user_id)
